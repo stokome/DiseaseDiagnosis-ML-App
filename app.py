64a519fd09beab240531d2da723af4fded59cd60
Yatharth <yatharthanand2410@gmail.com>
2023-01-17 07:17:44 -0800

    Updated app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import pickle
+import numpy as np
 import streamlit as st
 from streamlit_option_menu import option_menu
 
 diabetes_model = pickle.load(open('./saved_models/diabetes_model.sav', 'rb'))
+scaler = StandardScaler()
 
 with st.sidebar:
     menu =  option_menu('Diagnosis', ['Diabetes Diagnosis'], icons = ['activity'], default_index = 0)
@@ -24,32 +26,36 @@ if menu == 'Diabetes Diagnosis':
     
     with col1:
         skinT = st.number_input("Skin Thickness")
-    
-    with col2:
-        bmi = st.number_input("BMI value")
-
-    with col3:
-        diapedfn = st.number_input("Diabetes Pedigree Function")
-
-    with col1:
-        age = st.number_input("Age")
 
     with col2:
         insulin = st.number_input("Insulin Level")
+    
+    with col3:
+        bmi = st.number_input("BMI value")
 
-    inputs = [pregnancies, glucose, bloodP, skinT, insulin, bmi, diapedfn, age]
-    print(inputs)
+    with col1:
+        diapedfn = st.number_input("Diabetes Pedigree Function")
+
+    with col2:
+        age = st.number_input("Age")
+
+    
+
+    inputs = [[pregnancies, glucose, bloodP, skinT, insulin, bmi, diapedfn, age]]
     diabetes_res = ''
 
     if st.button("Check Diabetes"):
-        prediction = diabetes_model.predict([inputs])
+        prediction = diabetes_model.predict(inputs)
 
         if prediction == 1:
-            diabetes_res = 'Positive- Patient has Diabetes'
+            diabetes_res = 'Positive- Patient might have Diabetes'
+            st.error(diabetes_res)
+
         else:
             diabetes_res = 'Negative- Patient dosent have Diabetes'
+            st.success(diabetes_res)
 
-    st.success(diabetes_res)
+    
 
 
         
